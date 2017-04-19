d265617587bbcda2eb15e2c42c4baff0b9f17fe3
wayfrae <frazier.zach@gmail.com>
2017-04-18 19:36:35 -0600

    Update UserManagerTest.java

--- tests/UserManagerTest.java
+++ tests/UserManagerTest.java
@@ -9,7 +9,7 @@ import passProtect.UserManager;
 public class UserManagerTest {
 
 	@Test
-	public void testHashPasswordOnUserCreation() {
+	public void testHashPasswordOnConstructor() {
 		UserManager test = new UserManager("username", "testing");
 		UserManager test2 = new UserManager("user", "password");
 		assertEquals("dc724af18fbdd4e59189f5fe768a5f8311527050", test.getUserPass());
@@ -18,19 +18,22 @@ public class UserManagerTest {
 
 	@Test
 	public void testValidatePassword() {
-		UserManager test = new UserManager("username", "testing");
+		UserManager test = new UserManager("testValidatePassword", "testing");
 		test.createUser();
 		assertTrue(test.validatePassword());
 		
-		UserManager test2 = new UserManager("username", "Testing");
+		UserManager test2 = new UserManager("testValidatePassword", "Testing");
+		test2.createUser();
 		assertFalse(test2.validatePassword());
+		
+		test.removeUser();
 		
 	}
 	
 	@Test
 	public void testCreateUser() {
 		UserManager test = new UserManager("testCreateUser", "testing");
-		assertTrue("User Created", test.createUser());
+		assertTrue("User Not Created", test.createUser());
 		assertFalse("User duplicated", test.createUser());
 		test.removeUser();	
 		
@@ -41,8 +44,8 @@ public class UserManagerTest {
 		UserManager test = new UserManager("testRemoveUser", "testing");
 		test.createUser();
 		assertTrue("User Not Removed", test.removeUser());
-		assertFalse("User still exists", test.removeUser());	
-		test.removeUser();
+		assertFalse("User should not be found.", test.removeUser());	
+		
 	}
 	
 }
