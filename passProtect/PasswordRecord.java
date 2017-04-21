cdb5924e310efbdff37fd3d9daa9380f7e417452
wayfrae <frazier.zach@gmail.com>
2017-04-20 20:31:54 -0600

    Update PasswordRecord.java

--- passProtect/PasswordRecord.java
+++ passProtect/PasswordRecord.java
@@ -27,6 +27,38 @@ public class PasswordRecord {
 		this.username = user;
 		this.password = pass;
 	}
+
+	public String getDomain() {
+		return domain;
+	}
+
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public File getUserFile() {
+		return userFile;
+	}
+
+	public void setUserFile(File userFile) {
+		this.userFile = userFile;
+	}
 	
 	
 }
