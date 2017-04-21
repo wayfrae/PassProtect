c778e73e92dc9b2704c6b3580c2899f222204991
wayfrae <frazier.zach@gmail.com>
2017-04-20 20:31:02 -0600

    Update testPasswordManger.java

--- tests/testPasswordManger.java
+++ tests/testPasswordManger.java
@@ -2,15 +2,18 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+
 import org.junit.Test;
 
 import passProtect.PasswordManager;
+import passProtect.PasswordRecord;
 
 public class testPasswordManger {
 	
 	@Test
 	public void testAdd() {
-		PasswordManager user = new PasswordManager("filename.txt");
+		PasswordManager user = new PasswordManager("filename");
 		PasswordRecord pr = new PasswordRecord("domain", "user", "password");
 		user.add(pr);
 		assertTrue(user.getRecords().contains(pr));
